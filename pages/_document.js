7990984b9b428cc6bd49b3a640aec8c25b68a674
LeandKa <lcalvalcanti29@gmail.com>
2021-05-17 13:02:52 -0300

    some changes

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,20 @@ export default class MyDocument extends Document {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+            sheet.collectStyles(
+                <html>
+                <head>
+                  <link rel="preconnect" href="https://fonts.gstatic.com" />
+                  <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap"
+                    rel="stylesheet"
+                  />
+                </head>
+                <body style={{background:"#141414"}}>
+                  <App {...props} />
+                </body>
+              </html>
+            ),
         })
 
       const initialProps = await Document.getInitialProps(ctx)
